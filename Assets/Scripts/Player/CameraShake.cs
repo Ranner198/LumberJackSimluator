19e12f35a2579cbf866acd5e49dd87750a8c5c84
Ran Crump <jrc0051@animation.uah.edu>
2019-01-26 09:33:26 -0600

    ''

--- Assets/Scripts/Player/CameraShake.cs
+++ Assets/Scripts/Player/CameraShake.cs
@@ -2,6 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraShake : MonoBehaviour {
+public class CameraShake : MonoBehaviour
+{
 
+    public static CameraShake instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.                    //Store a reference to our BoardManager which will set up the level.
+    private int level = 3;                                  //Current level number, expressed in game as "Day 1".
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        /*
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+            */
+    }
 }
